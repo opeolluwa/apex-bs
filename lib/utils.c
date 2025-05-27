436e4a79501494d7042cd310bdfa793be5870593
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-05-27 20:58:58 +0100

    update

--- lib/utils.c
+++ lib/utils.c
@@ -4,6 +4,7 @@
 #include "../include/libraygui/raygui.h"
 #include "constants.h"
 
+#define MAX_INPUT_CHARS 99
 
 bool confirm_further_operation()
 {
@@ -16,18 +17,22 @@ bool confirm_further_operation()
 
 void render_create_bank_view(void)
 {
-    bool secret_view_active = true;
-    GuiLabel((Rectangle){10, VERTICAL_POSITION, WINDOW_WIDTH, 24}, "Welcome to account creation");
+    char first_name[MAX_INPUT_CHARS + 1] = "\0";
+    int account_type = 1;
 
-    // GuiTextInputBox((Rectangle){10, VERTICAL_POSITION * 2, (WINDOW_WIDTH * 0.65), (float)ELEMENT_HEIGHT}, "",
-    //                 "input your first name",
-    //                 "", "", 100, &secret_view_active);
+    SetWindowTitle("Apex Bank PLC - Create account");
 
-    GuiTextBox((Rectangle){10, VERTICAL_POSITION * 2, (WINDOW_WIDTH * 0.95), (float)ELEMENT_HEIGHT},
-               "Enter your first name", 99, false);
+    GuiTextBox((Rectangle){
+                   10, VERTICAL_POSITION * 1.5, (WINDOW_WIDTH * 0.95), (float)ELEMENT_HEIGHT
+               },
+               "enter your first name", 99, false);
 
-    GuiTextBox((Rectangle){10, VERTICAL_POSITION * 3, (WINDOW_WIDTH * 0.95), (float)ELEMENT_HEIGHT},
-               "Enter your last name", 99, false);
+    GuiTextBox((Rectangle){10, VERTICAL_POSITION * 2.5, (WINDOW_WIDTH * 0.95), (float)ELEMENT_HEIGHT},
+               "Enter your last name", 99, false
+    );
+
+    GuiDropdownBox((Rectangle){10, VERTICAL_POSITION * 3.5, (WINDOW_WIDTH * 0.95), (float)ELEMENT_HEIGHT},
+                   "Savings;current;investment", &account_type, false);
 }
 
 void render_transfer_funds_view(void)
