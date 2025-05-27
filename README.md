a6c8b54ab37ee6b53f524454d5dc0193c28bc557
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-05-27 12:44:14 +0100

    doc

---
+++ README.md
@@ -0,0 +1,37 @@
+# Apex Banking System
+
+## Getting Started
+
+```shell
+git clone https://github.com/opeolluwa/apex-bs
+```
+
+### Dependencies
+
+- [raylib](https://github.com/raysan5/raylib)
+- [cmake](https://cmake.org/)
+
+### Building locally
+
+```shell
+mkdir build
+cd build 
+cmake .. 
+make 
+```
+
+### Executing program
+```shell
+# from the build directory
+./apex_banking_system
+
+```
+
+## License
+
+This project is licensed under the [NAME HERE] License - see the LICENSE.md file for details
+
+## Acknowledgments
+
+Inspiration, code snippets, etc.
+        
--- main.c
+++ main.c
@@ -1,18 +1,57 @@
-#include <stdbool.h>
-#include "lib/utils.h"
-#include "lib/bank.h"
+// #include <stdbool.h>
+// #include "lib/utils.h"
+// #include "lib/bank.h"
+//
+// bool perform_more_transaction = false;
+//
+// int main(int argc, char* argv[])
+// {
+//     do
+//     {
+//         const enum BankOperation selected_operation = prompt_operation_selection();
+//         process_selection(selected_operation);
+//
+//         const bool selection = confirm_further_operation();
+//         perform_more_transaction = selection;
+//     }
+//     while (perform_more_transaction != false);
+// }
 
-bool perform_more_transaction = false;
 
-int main(int argc, char* argv[])
+
+
+#include "raylib.h"
+
+#define RAYGUI_IMPLEMENTATION
+#include "include/raygui.h"
+
+int main()
 {
-    do
-    {
-        const enum BankOperation selected_operation = prompt_operation_selection();
-        process_selection(selected_operation);
+    InitWindow(400, 200, "raygui - controls test suite");
+    SetTargetFPS(60);
 
-        const bool selection = confirm_further_operation();
-        perform_more_transaction = selection;
+    bool showMessageBox = false;
+
+    while (!WindowShouldClose())
+    {
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+        ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+
+        if (GuiButton((Rectangle){ 24, 24, 120, 30 }, "#191#Show Message")) showMessageBox = true;
+
+        if (showMessageBox)
+        {
+            int result = GuiMessageBox((Rectangle){ 85, 70, 250, 100 },
+                "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
+
+            if (result >= 0) showMessageBox = false;
+        }
+
+        EndDrawing();
     }
-    while (perform_more_transaction != false);
+
+    CloseWindow();
+    return 0;
 }
